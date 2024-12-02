162caf181655eda878cc81bbcf592e0a2123ec2e
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2024-12-02 11:29:55 -0800

    Add PostgreSQL Docker container and Django configuration
    Signed-off-by: Peter Ruszel <3279147+pruszel@users.noreply.github.com>

--- django/travel_stream_rest/settings.py
+++ django/travel_stream_rest/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'travel_stream_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'travel_stream_rest'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'postgres',
+        'PORT': 5432,
     }
 }
 
