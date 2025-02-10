a553fbaf544e4569873dcc70fd59ac45968e8ae4
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-09 23:43:55 -0800

    Implement routing with React Router and restructure main application layout
    Signed-off-by: Peter Ruszel <3279147+pruszel@users.noreply.github.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import Root from "./Root.tsx";
+import DashboardLayout from "./layouts/DashboardLayout.tsx";
+import Home from "./pages/Home.tsx";
+import TripList from "./pages/TripList.tsx";
+import NewTrip from "./pages/NewTrip.tsx";
+import Trip from "./pages/Trip.tsx";
+import AuthLayout from "./layouts/AuthLayout.tsx";
+import Login from "./pages/Login.tsx";
+import Register from "./pages/Register.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route element={<DashboardLayout />}>
+            <Route index element={<Home />} />
+            <Route path="trips" element={<TripList />}>
+              <Route path="new" element={<NewTrip />} />
+              <Route path=":tripid" element={<Trip />} />
+            </Route>
+          </Route>
+          <Route element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
