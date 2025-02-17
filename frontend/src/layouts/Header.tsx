bb3cd36e005dcad61239d976c9df75089cbc128e
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-17 14:48:36 -0800

    Update Header component with navigation links and styling
    Signed-off-by: Peter Ruszel <3279147+pruszel@users.noreply.github.com>

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,7 +1,20 @@
+import { OutlineButtonWithLink } from "../components/OutlineButtonWithLink";
+import { TextButtonWithLink } from "../components/TextButtonWithLink";
+
 export default function Header() {
   return (
     <>
-      <header></header>
+      <header className="z-10 px-8 py-4 shadow-md">
+        <div className="mx-auto flex max-w-[1400px] items-center justify-between">
+          <a href="/">Travel Stream</a>
+          <div className="flex gap-4">
+            <TextButtonWithLink to="/login">Login</TextButtonWithLink>
+            <OutlineButtonWithLink to="/register">
+              Register
+            </OutlineButtonWithLink>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
