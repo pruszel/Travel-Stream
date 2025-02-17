63939b2a908216c3a5c0d8a9dfe0a96620b44555
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-17 14:49:29 -0800

    Update DashboardLayout with Header component and improved styling
    Signed-off-by: Peter Ruszel <3279147+pruszel@users.noreply.github.com>

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router";
+import Header from "./Header";
 
 export default function DashboardLayout() {
   return (
     <>
-      <Outlet />
+      <div className="flex h-full flex-col">
+        <Header />
+        <main className="h-full bg-blue-50 p-8">
+          <div className="mx-auto max-w-[1400px]">
+            <Outlet />
+          </div>
+        </main>
+      </div>
     </>
   );
 }
