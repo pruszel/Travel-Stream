78d729ffea6699868758818994c8691461a6703d
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-19 16:51:16 -0800

    Add tests for Trip class to validate trip name, start date, and city names

--- frontend/src/models/Trip.test.ts
+++ frontend/src/models/Trip.test.ts
@@ -1,7 +1,54 @@
 import { describe, test, expect } from "vitest";
 import { Trip } from "./Trip";
 
-describe("Trip", () =>
+describe("Trip", () => {
+  test("it returns the trip name", () => {
+    const mockTrip: Trip = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    expect(mockTrip.name).toBe("Trip to Paris");
+  });
+
+  test("it returns the start date", () => {
+    const mockTrip: Trip = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      startDate: new Date("2023-10-01"),
+    });
+    expect(mockTrip.startDate).toEqual(new Date("2023-10-01"));
+  });
+
+  test("it returns the start city name", () => {
+    const mockTrip: Trip = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      startCity: "New York",
+    });
+    expect(mockTrip.startCity).toBe("New York");
+  });
+
+  test("it returns the end city name", () => {
+    const mockTrip: Trip = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      endCity: "Paris",
+    });
+    expect(mockTrip.endCity).toBe("Paris");
+  });
+
   test("it returns the number of days until trip starts", () => {
     const mockTripWithNoStartDate: Trip = new Trip({
       name: "Trip to Paris",
