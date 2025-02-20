15a241b511fc8d4347724b3b31924e4f3cf00449
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-19 22:21:39 -0800

    Display booked car rentals in UpcomingTripPanel with pluralization

--- frontend/src/components/UpcomingTripPanel.test.tsx
+++ frontend/src/components/UpcomingTripPanel.test.tsx
@@ -1,11 +1,15 @@
-import { describe, test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, test, expect, afterEach } from "vitest";
+import { cleanup, render, screen } from "@testing-library/react";
 import { UpcomingTripPanel } from "./UpcomingTripPanel";
 import { Trip } from "../models/Trip";
 
 describe("UpcomingTripPanel", () => {
+  afterEach(async () => {
+    cleanup();
+  });
+
   test("displays the trip name", () => {
-    const mockTrip: Trip = new Trip({
+    const mockTrip = new Trip({
       name: "Trip to Paris",
       id: "1",
       startDate: new Date(),
@@ -24,7 +28,7 @@ describe("UpcomingTripPanel", () => {
   });
 
   test("displays the start city name and the end city name", () => {
-    const mockTrip: Trip = new Trip({
+    const mockTrip = new Trip({
       name: "Trip to Paris",
       id: "1",
       startDate: new Date(),
@@ -41,7 +45,42 @@ describe("UpcomingTripPanel", () => {
   });
 
   test("displays the number of booked flights", () => {
-    const mockTrip: Trip = new Trip({
+    const mockTripWithNoBookedFlights = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      startDate: new Date(),
+      endDate: new Date(),
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    render(<UpcomingTripPanel trip={mockTripWithNoBookedFlights} />);
+    expect(screen.getByText("No flights")).toBeDefined();
+    cleanup();
+
+    const mockTripWithOneBookedFlight = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      startDate: new Date(),
+      endDate: new Date(),
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      flights: [
+        {
+          id: "1",
+          booked: true,
+          departureDate: new Date(),
+          departureTimezone: "America/Los_Angeles",
+          arrivalDate: new Date(),
+          arrivalTimezone: "America/Denver",
+        },
+      ],
+    });
+    render(<UpcomingTripPanel trip={mockTripWithOneBookedFlight} />);
+    expect(screen.getByText("1 flight booked")).toBeDefined();
+
+    const mockTripWithTwoBookedFlights = new Trip({
       name: "Trip to Paris",
       id: "1",
       startDate: new Date(),
@@ -76,7 +115,87 @@ describe("UpcomingTripPanel", () => {
         },
       ],
     });
-    render(<UpcomingTripPanel trip={mockTrip} />);
+    render(<UpcomingTripPanel trip={mockTripWithTwoBookedFlights} />);
     expect(screen.getByText("2 flights booked")).toBeDefined();
   });
+
+  test("displays the number of car rentals booked", () => {
+    const mockTripWithNoCarRentals = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      startDate: new Date(),
+      endDate: new Date(),
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    render(<UpcomingTripPanel trip={mockTripWithNoCarRentals} />);
+    expect(screen.getByText("No car rentals")).toBeDefined();
+
+    const mockTripWithOneBookedCarRental = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      startDate: new Date(),
+      endDate: new Date(),
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      carRentals: [
+        {
+          id: "1",
+          booked: true,
+          company: "Hertz",
+          pickUpDate: new Date(),
+          dropOffDate: new Date(),
+          pickUpLocation: "CDG",
+          dropOffLocation: "CDG",
+        },
+        {
+          id: "2",
+          booked: false,
+          company: "Enterprise",
+          pickUpDate: new Date(),
+          dropOffDate: new Date(),
+          pickUpLocation: "CDG",
+          dropOffLocation: "CDG",
+        },
+      ],
+    });
+    render(<UpcomingTripPanel trip={mockTripWithOneBookedCarRental} />);
+    expect(screen.getByText("1 car rental booked")).toBeDefined();
+
+    const mockTripWithTwoBookedCarRentals = new Trip({
+      name: "Trip to Paris",
+      id: "1",
+      startDate: new Date(),
+      endDate: new Date(),
+      ownerId: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      carRentals: [
+        {
+          id: "1",
+          booked: true,
+          company: "Hertz",
+          pickUpDate: new Date(),
+          dropOffDate: new Date(),
+          pickUpLocation: "CDG",
+          dropOffLocation: "CDG",
+        },
+        {
+          id: "2",
+          booked: true,
+          company: "Enterprise",
+          pickUpDate: new Date(),
+          dropOffDate: new Date(),
+          pickUpLocation: "CDG",
+          dropOffLocation: "CDG",
+        },
+      ],
+    });
+    render(<UpcomingTripPanel trip={mockTripWithTwoBookedCarRentals} />);
+    expect(screen.getByText("2 car rentals booked")).toBeDefined();
+  });
+
+  test("displays the number of booked hotels or vacation rental nights", () => {});
 });
--- frontend/src/components/UpcomingTripPanel.tsx
+++ frontend/src/components/UpcomingTripPanel.tsx
@@ -1,4 +1,5 @@
 import { Trip } from "../models/Trip";
+import pluralize from "pluralize";
 
 export function UpcomingTripPanel({ trip }: { trip: Trip | null }) {
   if (!trip) {
@@ -9,13 +10,25 @@ export function UpcomingTripPanel({ trip }: { trip: Trip | null }) {
     );
   }
 
+  const bookedFlightCount = trip.getBookedFlightCount();
+  const bookedCarRentalCount = trip.getBookedCarRentalCount();
+
   return (
     <>
       <h2>{trip.name}</h2>
       {trip.startDate ? <p>{trip.getDaysUntilTripStarts()} days away</p> : null}
       {trip.startCity ? <p>{trip.startCity}</p> : null}
       {trip.endCity ? <p>{trip.endCity}</p> : null}
-      <p>{trip.getBookedFlightCount()} flights booked</p>
+      <p>
+        {bookedFlightCount
+          ? `${bookedFlightCount} ${pluralize("flight", bookedFlightCount)} booked`
+          : `No flights`}
+      </p>
+      <p>
+        {bookedCarRentalCount
+          ? `${bookedCarRentalCount} car ${pluralize("rental", bookedCarRentalCount)} booked`
+          : `No car rentals`}
+      </p>
     </>
   );
 }
