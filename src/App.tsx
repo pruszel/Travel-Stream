7724e1799e0ac5dbb553be77ff7b5ef57dc03ebd
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-27 12:04:34 -0800

    Update App.tsx with daisyUI button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import React from "react";
 
 function App() {
+  const [clicked, setClicked] = React.useState(false);
+
   return (
     <>
       <h1 className="text-3xl font-bold">Hello, World!</h1>
+      <br />
+      <button className="btn" disabled={clicked} onClick={() => {setClicked(true);}}>{clicked ? "Thanks!" : "Click Me!"}</button>
     </>
+  );
 }
 
 export default App;
