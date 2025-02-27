eab7a3c44609ea98f254dfb430616aef33fde0a9
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-27 03:05:23 -0800

    Add test for App component

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import { describe, it, expect, afterEach } from "vitest";
+import { cleanup, render, screen } from "@testing-library/react";
+import App from "./App.tsx";
+
+describe("App", () => {
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("renders the App component", () => {
+    render(<App />);
+    expect(screen.getByText("Hello, World!")).toBeDefined();
+  });
+});
