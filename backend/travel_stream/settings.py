a8d5da8d1504827af69362e769c716ce01363e1d
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-03-11 12:43:57 -0700

    Add users app to installed apps list and set AUTH_USER_MODEL to custom user model

--- backend/travel_stream/settings.py
+++ backend/travel_stream/settings.py
@@ -33,12 +33,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # custom apps
+    "apps.users",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
