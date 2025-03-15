98b951ab6548efe1d1538003e56069a0f954e3d5
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-03-15 16:42:34 -0700

    Fix formatting issues with Prettier

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
   plugins: [react()],
-})
+});
