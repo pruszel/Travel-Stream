15c944660ce114dce77baf79ff738f2624395728
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-03-16 14:05:43 -0700

    Add default value for ALLOWED_HOSTS setting

--- backend/travel_stream/settings.py
+++ backend/travel_stream/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS", cast=parse_comma_separated_str)
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", cast=parse_comma_separated_str, default=[])
 
 
 # Application definition
