5b987dc578d8754cf523f26724965b15dfcfd8b6
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-03-15 21:51:47 -0700

    Add sample daisyUI component to verify functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [buttonText, setButtonText] = useState("Click me");
+  const handleClick = () => {
+    setButtonText("Thanks!");
+  };
+
   return (
     <>
       <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <button className="mt-4 btn" onClick={handleClick}>
+        {buttonText}
+      </button>
     </>
   );
 }
