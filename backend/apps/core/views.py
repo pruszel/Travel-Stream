106caadc0dd3559f94fe7e3e7b3eeb96e3fa81ad
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-03-16 21:02:57 -0700

    Fix formatting issues

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -3,9 +3,11 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST, require_safe
 from django.middleware.csrf import get_token
 
+
 @require_safe
 def get_csrf_token(request):
     return JsonResponse({"csrf_token": get_token(request)})
+
 
 @require_POST
 def test_post(request):
--- backend/travel_stream/settings.py
+++ backend/travel_stream/settings.py
@@ -149,10 +149,10 @@ CSRF_TRUSTED_ORIGINS = env(
     "CSRF_TRUSTED_ORIGINS", cast=parse_comma_separated_str, default=[]
 )
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
 SESSION_COOKIE_SECURE = True  # requires HTTPS except on localhost
 SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SAMESITE = 'Lax'
+SESSION_COOKIE_SAMESITE = "Lax"
 SESSION_COOKIE_AGE = 43200  # 12 hours in seconds
 
 ADMINS = env("ADMINS", cast=parse_name_email_pair_str, default=[])
