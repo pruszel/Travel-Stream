925c654444b58e25d4122651ac645b0e555584bb
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-03-16 20:50:00 -0700

    Add view getting CSRF token and view for testing a POST request with CSRF protection

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render  # noqa: F401
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST, require_safe
+from django.middleware.csrf import get_token
 
-# Create your views here.
+@require_safe
+def get_csrf_token(request):
+    return JsonResponse({"csrf_token": get_token(request)})
+
+@require_POST
+def test_post(request):
+    return JsonResponse({"message": "Hello, world!"})
