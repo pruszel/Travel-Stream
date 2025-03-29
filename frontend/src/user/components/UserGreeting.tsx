da90dad6e6825ecc023bda28f5c533e1f31df019
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-03-29 08:45:30 -0700

    Fix formatting issues

--- frontend/src/user/components/UserGreeting.tsx
+++ frontend/src/user/components/UserGreeting.tsx
@@ -1,9 +1,7 @@
 // UserGreeting.tsx
 
-import {User} from "firebase/auth";
+import { User } from "firebase/auth";
 
 export function UserGreeting({ user }: { user: User }) {
-    return (
-        <p>Hello, {user.displayName}</p>
-    );
+  return <p>Hello, {user.displayName}</p>;
 }
