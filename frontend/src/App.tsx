6531e32e194900ab1c5d57c38ef97e38787457ec
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-03-29 08:36:46 -0700

    Move AuthDisplay component to users directory with tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import "./App.css";
-import {
-  useAuthState,
-} from "react-firebase-hooks/auth";
-import { auth } from "@user/utils/firebase";
-import { User } from "firebase/auth";
-import {SignOutButton} from "@user/components/SignOutButton.tsx";
-import {SignInWithGoogle} from "@user/components/SignInWithGoogle.tsx";
-import { UserGreeting } from "@user/components/UserGreeting";
+// App.tsx
 
-function App() {
+import "./App.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@user/utils/firebase";
+import { AuthDisplay } from "@user/components/AuthDisplay.tsx";
+
+export function App() {
   const [user, loading, error] = useAuthState(auth);
 
   return (
@@ -17,35 +14,3 @@ function App() {
     </>
   );
 }
-
-function AuthDisplay({
-  user,
-  loading,
-}: {
-  user: User | null | undefined;
-  loading: boolean;
-  error: Error | undefined;
-}) {
-  if (user) {
-    return (
-      <>
-        <div className="flex gap-8 items-center flex-col">
-          <UserGreeting user={user} />
-          <SignOutButton />
-        </div>
-      </>
-    );
-  }
-
-  if (loading) {
-    return;
-  }
-
-  return (
-    <>
-      <SignInWithGoogle />
-    </>
-  );
-}
-
-export default App;
