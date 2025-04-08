e963e5fc3bdbeb22cf4ee0647e9f8ee82add0cc1
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-04-08 08:43:30 -0700

    Create basic Trip model

--- backend/apps/trips/models.py
+++ backend/apps/trips/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+import apps.core.models
+
+
+class Trip(apps.core.models.BaseModel):
+    """
+    Trip model to store basic trip information.
+    """
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    destination = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
