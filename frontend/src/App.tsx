f89a0b682412bf0dfa0bee8330d61963db498824
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-04-08 00:02:45 -0700

    Fix inconsistency in import path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@
 import "./App.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/user/utils/firebase";
-import { AuthDisplay } from "@/user/components/AuthDisplay.tsx";
+import { AuthDisplay } from "@/user/components/AuthDisplay";
 
 export function App() {
   const [user, loading, error] = useAuthState(auth);
