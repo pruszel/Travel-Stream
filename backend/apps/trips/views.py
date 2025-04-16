85faf0e68074a29079b39110bf162f56047c971c
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-04-15 19:52:42 -0700

    Add DRF viewset for Trip serializer

--- backend/apps/trips/views.py
+++ backend/apps/trips/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# backend/apps/trips/views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Trip
+from .serializers import TripSerializer
+from apps.users.permissions import IsOwner
+
+
+class TripViewSet(viewsets.ModelViewSet):
+    serializer_class = TripSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+    owner_field = "created_by"
+
+    def get_queryset(self):
+        # Return only the trips owned by the current user.
+        return Trip.objects.filter(created_by=self.request.user)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
