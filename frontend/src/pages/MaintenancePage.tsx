07f92987132182dbde25d4028374529a8471252e
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-04-21 12:14:07 -0700

    Add MaintenancePage

---
+++ frontend/src/pages/MaintenancePage.tsx
@@ -0,0 +1,14 @@
+// frontend/src/pages/MaintenancePage.tsx
+
+export function MaintenancePage() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-4">Maintenance Mode</h1>
+        <p className="text-xl">
+          The application is currently under maintenance.
+        </p>
+      </div>
+    </>
+  );
+}
