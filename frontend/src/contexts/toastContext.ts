b1d468c5b85ccbe340699ec6063e83db8f705ab9
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-04-21 12:09:40 -0700

    Add toast (UI message) context and provider

---
+++ frontend/src/contexts/toastContext.ts
@@ -0,0 +1,33 @@
+// frontend/src/contexts/toastContext.ts
+
+import { createContext } from "react";
+
+export type ToastType = "success" | "error" | "warning" | "info";
+
+export interface Toast {
+  id: number;
+  type: ToastType;
+  message: string;
+  expirationInSeconds?: number | null;
+}
+
+export interface ToastContextType {
+  toasts: Toast[];
+  addToast: (
+    type: ToastType,
+    message: string,
+    expirationInSeconds?: number,
+  ) => void;
+  removeToast: (id: number) => void;
+}
+
+const defaultToasts: Toast[] = [];
+
+const defaultToastContext: ToastContextType = {
+  toasts: defaultToasts,
+  addToast: () => {},
+  removeToast: () => {},
+};
+
+export const ToastContext =
+  createContext<ToastContextType>(defaultToastContext);
