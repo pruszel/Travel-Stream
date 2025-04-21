2b6c5f94d4a9b908fa2d24b0348b1ebe856aa181
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-04-21 12:05:01 -0700

    Add global constants

---
+++ frontend/src/constants.ts
@@ -0,0 +1,9 @@
+// frontend/src/constants.ts
+
+export const DEFAULT_TOAST_EXPIRATION_IN_SECONDS = 10;
+
+export const AUTH_ERROR_MESSAGE = `Something went wrong. Try signing in with Google again. If the problem persists, contact support at <a href="mailto:support@travelstreamapp.com">support@travelstreamapp.com</a>.`;
+
+export const LD_CLIENT_ID = import.meta.env.PROD
+  ? `67f0bff500b7a80955249fc7`
+  : `67f0bff500b7a80955249fc6`;
