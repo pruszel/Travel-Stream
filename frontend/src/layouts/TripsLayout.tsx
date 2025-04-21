0a8370206d73da72f0e2c1d06101745c661a0b31
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-04-21 12:12:54 -0700

    Add TripsLayout for all trips pages

---
+++ frontend/src/layouts/TripsLayout.tsx
@@ -0,0 +1,50 @@
+// frontend/src/layouts/TripsLayout.tsx
+
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router";
+
+import { AUTH_ERROR_MESSAGE } from "@/constants.ts";
+import { ToastContext } from "@/contexts/toastContext.ts";
+import { ToastProvider } from "@/contexts/toastProvider.tsx";
+import { Toast } from "@/components/Toast.tsx";
+import { Header } from "@/components/Header.tsx";
+import { ContentWrapper } from "@/components/ContentWrapper.tsx";
+import { AuthContext } from "@/contexts/authContext.ts";
+
+export function TripsLayout() {
+  const { firebaseUser, isAuthStateLoading, authError } =
+    useContext(AuthContext);
+  const { addToast } = useContext(ToastContext);
+
+  // Redirect to login page if not authenticated
+  if (!firebaseUser) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // Show loading state
+  if (isAuthStateLoading) {
+    return <div className="loading loading-spinner loading-lg"></div>;
+  }
+
+  if (authError) {
+    console.error(`Error during sign in with Google: ${authError}`);
+    addToast("error", AUTH_ERROR_MESSAGE);
+  }
+
+  return (
+    <>
+      <div className="py-8 grid grid-cols-1 justify-stretch max-w-4xl mx-auto">
+        <ToastProvider>
+          <Toast />
+        </ToastProvider>
+        <Header />
+
+        <ContentWrapper>
+          <main className="flex flex-col">
+            <Outlet context={{ firebaseUser: firebaseUser }} />
+          </main>
+        </ContentWrapper>
+      </div>
+    </>
+  );
+}
