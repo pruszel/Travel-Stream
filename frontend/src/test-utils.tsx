ffb3400feb8eaee96ddc313654c9d78ad34596b0
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-04-30 15:11:31 -0700

    test: add utils file with mocks

---
+++ frontend/src/test-utils.tsx
@@ -0,0 +1,41 @@
+// frontend/src/test-utils.tsx
+
+import { vi } from "vitest";
+import { User } from "firebase/auth";
+import { Analytics } from "firebase/analytics";
+import { AuthContextType } from "@/contexts/authContext";
+
+export const mockAnalytics = {} as Analytics;
+
+export const mockSignInWithGoogle = vi.fn().mockResolvedValue({});
+export const mockGetIdToken = vi.fn().mockResolvedValue("fake-token");
+const mockSignOut = vi.fn();
+
+export const authContextLoading: AuthContextType = {
+  firebaseUser: null,
+  isAuthStateLoading: true,
+  authError: undefined,
+  signInWithGoogle: mockSignInWithGoogle,
+  signOut: mockSignOut,
+};
+
+export const authContextLoggedOut: AuthContextType = {
+  firebaseUser: null,
+  isAuthStateLoading: false,
+  authError: undefined,
+  signInWithGoogle: mockSignInWithGoogle,
+  signOut: mockSignOut,
+};
+
+const mockUser = {
+  uid: "12345",
+  getIdToken: mockGetIdToken,
+} as unknown as User;
+
+export const authContextLoggedIn: AuthContextType = {
+  firebaseUser: mockUser,
+  isAuthStateLoading: false,
+  authError: undefined,
+  signInWithGoogle: mockSignInWithGoogle,
+  signOut: mockSignOut,
+};
