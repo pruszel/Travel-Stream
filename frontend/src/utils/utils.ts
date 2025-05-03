3e3973a5244c6e1dabeecd750353b7bfeee2aec3
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-05-02 23:06:57 -0700

    docs: add descriptive comment for utils function

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,5 +1,8 @@
 // frontend/src/utils/utils.ts
 
+/**
+ * Type-safe convert FormDataEntryValue to string.
+ */
 export function convertFormDataToStringSafely(
   value: FormDataEntryValue | null | undefined,
 ): string {
