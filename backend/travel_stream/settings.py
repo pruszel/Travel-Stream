74f35c64bb8ac98e987a7f6652a3920f01c637e6
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-05-06 23:36:15 -0700

    feat: enable logging
    messages from the django logger of level INFO or higher are logged to
    the console

--- backend/travel_stream/settings.py
+++ backend/travel_stream/settings.py
@@ -178,6 +178,27 @@ REST_FRAMEWORK = {
     ],
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
+
 # Fly.io specific settings
 if env("FLY_APP_NAME", default=None):
     # Redirect HTTP to HTTPS
