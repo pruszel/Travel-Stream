4a899c68b1baba31d602427d5d541766dc53eac0
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-05-08 22:58:52 -0700

    feat: add serializer for Flight model and use it for Trip serializer
    The `flights` field of the `TripSerializer` is **read-only**. This means
    when a client makes a GET request to /trips or /trips/:id then all
    associated flights will be returned along with the trip fields. But it
    is not possible to make a PATCH request to /trips/:id with the `flights`
    field in the body. This would require additional complexity when saving
    the nested/related Flight model. To avoid this, the Flight model must be
    used for write purposes.

--- backend/apps/trips/serializers.py
+++ backend/apps/trips/serializers.py
@@ -1,10 +1,54 @@
 # backend/apps/trips/serializers.py
 
 from rest_framework import serializers
-from .models import Trip
+from .models import Trip, Flight
 
+
+class FlightSerializer(serializers.ModelSerializer):
+    trip_id = serializers.IntegerField()
+
+    class Meta:
+        model = Flight
+        fields = [
+            "id",
+            "trip_id",
+            "airline",
+            "confirmation_number",
+            "flight_number",
+            "departure_airport",
+            "arrival_airport",
+            "departure_time",
+            "arrival_time",
+        ]
+        read_only_fields = ["created_at", "updated_at", "created_by"]
+
+    # noinspection PyMethodMayBeStatic
+    def validate_trip_id(self, trip_id):
+        """Make sure a trip with this id exists.
+
+        This method is called automatically by DRF when validating the serializer.
+        """
+        try:
+            Trip.objects.get(id=trip_id)
+        except Trip.DoesNotExist:
+            raise serializers.ValidationError(f"Trip id {trip_id} does not exist.")
+        return trip_id
+
+    def create(self, validated_data):
+        # Add the user as created_by
+        user = self.context["request"].user
+        validated_data["created_by"] = user
+
+        # Get the trip object using the trip_id
+        trip_id = validated_data.pop("trip_id")
+        trip = Trip.objects.get(id=trip_id)
+        validated_data["trip"] = trip
+
+        return super().create(validated_data)
 
 class TripSerializer(serializers.ModelSerializer):
+    flights = FlightSerializer(many=True, read_only=True)
+
     class Meta:
         model = Trip
         fields = [
@@ -16,12 +60,13 @@ class TripSerializer(serializers.ModelSerializer):
             "end_date",
             "created_at",
             "updated_at",
+            "flights"
         ]
         read_only_fields = ["created_at", "updated_at", "created_by"]
 
     def create(self, validated_data):
-        # Get the current user from the request
-        user = self.context["request"].user
         # Add the user as created_by
+        user = self.context["request"].user
         validated_data["created_by"] = user
+
         return super().create(validated_data)
