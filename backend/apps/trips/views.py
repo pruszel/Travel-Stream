80296cde45d5f880bea14e7e76d9b8ee29b2f0ad
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-05-08 23:04:05 -0700

    feat: add generalized viewset as well as specialized Trip and Flight viewsets

--- backend/apps/trips/views.py
+++ backend/apps/trips/views.py
@@ -2,22 +2,22 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .models import Trip
-from .serializers import TripSerializer
+from .models import Trip, Flight
+from .serializers import TripSerializer, FlightSerializer
 from apps.users.permissions import IsOwner
 
 
-class TripViewSet(viewsets.ModelViewSet):
-    serializer_class = TripSerializer
+class OwnerViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwner]
     owner_field = "created_by"
 
     def get_queryset(self):
-        """Return only the trips owned by the current user."""
-        return Trip.objects.filter(created_by=self.request.user)
+        """Return only objects owned by the current user."""
+        model_class = self.serializer_class.Meta.model
+        return model_class.objects.filter(created_by=self.request.user)
 
     def get_object(self):
-        """Override get_object to ensure the user is the owner of the trip."""
+        """Override get_object to ensure the user is the owner of the object."""
         queryset = self.get_queryset()
         obj = get_object_or_404(queryset, pk=self.kwargs["pk"])
         self.check_object_permissions(self.request, obj)
@@ -28,3 +28,23 @@ class TripViewSet(viewsets.ModelViewSet):
         context = super().get_serializer_context()
         context["request"] = self.request
         return context
+
+
+class TripViewSet(OwnerViewSet):
+    serializer_class = TripSerializer
+    queryset = Trip.objects.all()
+
+
+class FlightViewSet(OwnerViewSet):
+    serializer_class = FlightSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned flights to a given trip,
+        by filtering against a `trip_id` query parameter in the URL.
+        """
+        queryset = Flight.objects.all()
+        trip_id = self.request.query_params.get("trip_id")
+        if trip_id is not None:
+            queryset = queryset.filter(trip_id=trip_id)
+        return queryset
