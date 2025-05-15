50fdd7c7af0357401bbd4c78c39d24fbef441f65
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-05-15 11:28:39 -0700

    feat: add accessibility attributes to loading screen component

--- frontend/src/components/LoadingScreen.tsx
+++ frontend/src/components/LoadingScreen.tsx
@@ -3,7 +3,14 @@
 export function LoadingScreen() {
   return (
     <>
-      <div className="h-screen w-screen flex items-center justify-center">
+      <div
+        className="h-screen w-screen flex items-center justify-center"
+        role="progressbar"
+        aria-valuetext="Loading…"
+        aria-live="assertive"
+        aria-valuemin={0}
+        aria-valuemax={100}
+      >
         <div className="loading loading-spinner loading-xl"></div>
       </div>
     </>
